10430bd6f72f97b95336b126dc0ce5d67c02c7c8
Marcello Costa Saldanha <marcellosaldanha7@gmail.com>
2015-03-05 09:41:28 -0300

    alterei

--- main.c
+++ main.c
@@ -2,6 +2,15 @@
 #include <stdlib.h>
 
 int main(void){
-	printf("Hello World!");
+	FILE *f;
+	int num_mat;
+	char nome[100];
+	printf("Digite a matricula:\n");
+	scanf("%i", &num_mat);
+	printf("Digite o nome:\n");
+	scanf("%s", nome);
+	f = fopen("saida.txt", "w");
+	fprintf(f, "%d %s\n", num_mat, nome);
+	fclose(f);
 	return;
 }
