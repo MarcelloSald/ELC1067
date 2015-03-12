1073761506429a231277dc6fcf04922e2414a5d7
Marcello Costa Saldanha <msaldanha@inf.ufsm.br>
2015-03-11 21:01:49 -0300

    edição

--- T1/T1.c
+++ T1/T1.c
@@ -1,36 +1,45 @@
-int main(int argc, char **argv){
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(void){
 	char *nome;
 	int matriculas[50];
 	char nomes[50][50];
 	int n;
-	void le_alunos(int* matriculas,char** nomes,int* &n);{
-		FILE *f= fopen("alunos.txt", "r");
+	FILE *f=fopen("aluno.txt", "r");
+	if (f==NULL){
+		printf("Arquivo inexistente\n\n");
+		return 1;
+	}
+	FILE *f2=("notas.txt", "r");
+	if (f == NULL){
+		printf("Arquivo de notas inexistente\n\n");
+		return 1;
+	}
+	void le_alunos(int* matriculas,char** nomes,int* &n){
 		int mat; char c; char nomes[50];int i, int linha;
-		while (feof(f) !=0){
-			if(fscanf(f, "%d", &mat) ==0){
-				break;
-				i=0;
-				}	
-				fscanf(f, "%d", &mat);
-				c= fgetc(f);
-				while(c != '\n'){
-					nome[i]=c;
-					c=fgetc(f);
-					i++;
-				}
-				nome[i]= '\0';
-				matriculas[linha]=mat;
-				ctrcpy(nomes[linha], nome);
-				linha++;
-			}
+		if(fscanf(f, "%d", &mat) ==0){
+			break;
+			i=0;
+		}
+		fscanf(f, "%d", &mat);
+		c= fgetc(f);
+		while(c != '\n'){
+			nome[i]=c;
+			c=fgetc(f);
+			i++;
+		}
+		nome[i]= '\0';
+		matriculas[linha]=mat;
+		strcpy(nomes[linha], nome);
+		linha++;
 		*n = linha;
 		fclose(f);
-
-	if (argc>1){
-		nome=argv[1];
+		if (argc>1){
+			nome=argv[1];
+		}
+		printf("%s\n", nome);
 	}
-	printf("%s\n", nome);
-
-
-
 }
+
